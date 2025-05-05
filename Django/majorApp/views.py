4e21ae51f1baf0ec8bf8ead502c60224ea9d1701
Josh <Joshmono2008@outlook.com>
2025-05-05 21:10:47 +1000

    Dashboard

--- Django/majorApp/views.py
+++ Django/majorApp/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-
+from .models import Company, Lead, Quote, Invoice
 
 # Create your views here.
 @login_required
-def index(request):
+def dashboard(request):
     context = {}
-    return  HttpResponse("test")
+
+    companies = Company.objects.all()
+    leads = Lead.objects.all()
+    quotes = Quote.objects.all()
+    invoices = Invoice.objects.all()
+
+    context['companies'] = companies
+    context['leads'] = leads
+    context['quotes'] = quotes
+    context['invoices'] = invoices
+
+    return render(request, "majorApp/dashboard.html", context)
